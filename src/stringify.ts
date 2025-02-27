d568b80a3b29cfd9406a95ddf8fc483f161dade0
Cristian Díaz <xtndiaz@gmail.com>
2025-02-27 14:25:24 -0500

    Supplement hexString with optional alpha

--- src/stringify.ts
+++ src/stringify.ts
@@ -1,4 +1,14 @@
-export const hexString = (value: number) => {
-  const _hexString = value.toString(16)
-  return '0'.repeat(6 - _hexString.length) + _hexString
+import { clamp } from "./math"
+
+export const hexString = (colorValue: number, alpha: number = 1) => {
+  const abbrHexString = colorValue.toString(16)
+  let fullHexString = '0'.repeat(6 - abbrHexString.length) + abbrHexString
+  
+  const _alpha = clamp(alpha, 0, 1)
+  if (_alpha !== 1) {
+    const alphaHexString = Math.round(255 * _alpha).toString(16)
+    fullHexString += '0'.repeat(2 - alphaHexString.length) + alphaHexString
+  }
+  
+  return fullHexString
 }
