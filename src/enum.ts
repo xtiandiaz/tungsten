051b9d2dbd47634786288c4dd75c27d7e021d410
Cristian Díaz <xtndiaz@gmail.com>
2025-05-15 21:24:19 -0500

    Rename randomness utils and refactor generic definition type of enumKeyFromValue

--- src/enum.ts
+++ src/enum.ts
@@ -1,6 +1,4 @@
-export type StringEnum<T> = Record<keyof T, string>
-
-export function enumKeyFromValue<T extends StringEnum<T>>(enumType: T, value: number | string): T[keyof T] | undefined {
+export function enumKeyFromValue<T extends Record<keyof T, number | string>>(enumType: T, value: number | string): T[keyof T] | undefined {
   const index = Object.values(enumType).indexOf(value as unknown as T)
   return enumType[Object.keys(enumType)[index] as keyof T]
 }
