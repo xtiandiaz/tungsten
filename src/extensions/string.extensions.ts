b9833ac8f25b0d4f9d2f0e8c71054961b83b8db9
Cristian Díaz <xtndiaz@gmail.com>
2025-08-16 17:04:15 +0300

    Add reg-exp file and escape string extension

--- src/extensions/string.extensions.ts
+++ src/extensions/string.extensions.ts
@@ -1,12 +1,19 @@
+import { SPECIAL_CHARS } from '../reg-exp'
+
 declare global {
   interface String {
     capitalized(): string
+    escape(): string
     removeLeadingAndTrailingSpaces(): string  
   }
 }
 
 String.prototype.capitalized = function(this: string): string {
   return this.slice(0, 1).toLocaleUpperCase() + this.slice(1)
+}
+
+String.prototype.escape = function(this: string): string {
+  return this.replace(SPECIAL_CHARS, '\\$&')
 }
 
 String.prototype.removeLeadingAndTrailingSpaces = function(this: string): string {
